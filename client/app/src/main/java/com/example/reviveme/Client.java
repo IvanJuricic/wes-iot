04754727881e132f472d24895fe24fc5bf8954a7
IvanJuricic <ivanjuricic97@gmail.com>
2020-12-16 18:05:14 +0100

    Add two way comm

---
+++ client/app/src/main/java/com/example/reviveme/Client.java
@@ -0,0 +1,130 @@
+package com.example.reviveme;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class Client implements Runnable {
+
+    static final String TAG = "IVAN";
+    private DatagramSocket socket;
+    private String ip;
+    private Thread send, receive, listen;
+    private int port;
+    boolean currState;
+
+    protected MainActivity context;
+
+    // constructor
+    public Client(String IP, int PORT, Context context) throws UnknownHostException {
+        this.ip = IP;
+        this.port = PORT;
+        this.context = (MainActivity) context;
+    }
+
+    @Override
+    public void run(){
+        try {
+            socket = new DatagramSocket();
+            //ip = InetAddress.getByName(address);
+            socket.setBroadcast(true);
+            socket.setReuseAddress(true);
+            String connection = "Master here!";
+            send(connection);
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        listen();
+
+    }
+
+
+    // opening connection
+    public boolean openConnection() {
+
+        run();
+        return true;
+    }
+
+    // receiving messages
+    // waits for a packet
+    public String receive() {
+        byte[] data = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(data, data.length);
+        while (true) {
+            try {
+                socket.receive(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String message = new String(packet.getData());
+            return message;
+        }
+
+    }
+
+    // sending message over UDP
+    public void send(final String msg) {
+        send = new Thread("Send") {
+            @Override
+            public void run() {
+                DatagramPacket packet = null;
+                byte[] bytes = msg.getBytes();
+                try {
+                    packet = new DatagramPacket(bytes, bytes.length, InetAddress.getByName(ip),port);
+
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    socket.send(packet);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        send.start();
+    }
+
+    // listen method
+    public void listen() {
+        Log.d(TAG, "Listening...");
+        listen = new Thread("Listen") {
+            @Override
+            public void run() {
+                while (true) {
+                    String message = receive();
+                    if (message.contains("Shock completed!")) {
+                        context.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                context.animation.setSpeed(1);
+                                context.animation.playAnimation();
+                            }
+                        });
+                    } else if(message.contains("UNSHOCKED")) {
+                        context.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                context.animation.setSpeed(-1);
+                                context.animation.playAnimation();
+                                context.shockBtn.setEnabled(true);
+                            }
+                        });
+                    }
+                }
+            }
+        };
+        listen.start();
+    }
+
+}
--- client/app/src/main/java/com/example/reviveme/MainActivity.java
+++ client/app/src/main/java/com/example/reviveme/MainActivity.java
@@ -2,13 +2,73 @@ package com.example.reviveme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.airbnb.lottie.LottieAnimationView;
+
+import java.net.UnknownHostException;
+
+public class MainActivity extends AppCompatActivity{
+
+    private final static String TAG = "6G Only";
+
+    LottieAnimationView animation;
+    Button shockBtn;
+
+    private Client client;
+
+    Thread listen, run, connect;
+
+    String HOST_IP = "192.168.43.223";
+    int HOST_PORT = 3000;
+
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = MainActivity.this;
+
+        shockBtn = findViewById(R.id.shock_button);
+
+        animation = (LottieAnimationView)findViewById(R.id.animationView);
+
+        Thread t = null;
+        try {
+            t = new Thread(client = new Client(HOST_IP, HOST_PORT,mContext));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        t.start();
+
     }
+
+    public void shock(View view) {
+
+        String message = "SHOCK!";
+
+        send(message);
+
+        shockBtn.setEnabled(false);
+
+
+
+        //animation.playAnimation();
+
+    }
+
+    public void send(String message) {
+        client.send(message);
+    }
+
+
 }
